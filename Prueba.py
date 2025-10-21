6c22d798ca34a1744d47ccde8f4bee766b2b39bf
Alejandro <alvardoortizalejadrodario@gmail.com>
2025-10-21 10:16:54 -0600

    Proyecto #1

--- Prueba.py
+++ Prueba.py
@@ -123,7 +123,7 @@ print(train_X.shape, valid_X.shape, train_label.shape, valid_label.shape)
 # ----------------------------------------------------------------
 
 INIT_LR = 1e-3
-epochs = 5 # Aumentado para mejor entrenamiento
+epochs = 10 # Aumentado para mejor entrenamiento
 batch_size = 64
 
 sport_model = Sequential()
@@ -157,5 +157,4 @@ sport_train = sport_model.fit(train_X, train_label, batch_size=batch_size,epochs
 sport_model.save("sports_mnist_2.h5")
 # evaluar la red
 test_eval = sport_model.evaluate(test_X, test_Y_one_hot, verbose=1)
-
 
